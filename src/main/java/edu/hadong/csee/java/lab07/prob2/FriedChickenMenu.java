89a60792e11d840f0ee59fab464ff5443481a071
ZEHCARIAH <21700570@handong.edu>
2018-03-31 13:35:35 +0900

    add public string method

--- src/main/java/edu/hadong/csee/java/lab07/prob2/FriedChickenMenu.java
+++ src/main/java/edu/hadong/csee/java/lab07/prob2/FriedChickenMenu.java
@@ -2,4 +2,18 @@ package edu.hadong.csee.java.lab07.prob2;
 
 public class FriedChickenMenu {
 
+	private String name;
+	private double price;
+	private double stars;
+	
+	public FriedChickenMenu()
+	{
+		this.name="";
+		this.price=0.0;
+		this.stars=0;
+		}
+	public String get_name() 
+	{
+		return this.name;
+	}
 }
