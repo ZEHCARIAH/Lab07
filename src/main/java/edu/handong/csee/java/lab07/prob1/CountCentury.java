d3ad116e9f8d973124a781f8956d9ce2cd8e0a6e
ZEHCARIAH <21700570@handong.edu>
2018-03-31 11:25:52 +0900

    add public integer method

--- src/main/java/edu/handong/csee/java/lab07/prob1/CountCentury.java
+++ src/main/java/edu/handong/csee/java/lab07/prob1/CountCentury.java
@@ -10,4 +10,13 @@ public class CountCentury {
 	public CountCentury(int year) {
 		this.year=year;
 	}
+	public int Cal_CountCentury() {
+		if(year%100==0)
+			year=year/100;
+		else
+		{
+			year=(year/100)+1;
+		}
+		return year;
+	}
 }
