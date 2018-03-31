2612bd9ed9b9bbeeed955de105157b7d53951d33
ZEHCARIAH <21700570@handong.edu>
2018-03-31 22:55:34 +0900

    add sing line comments

--- src/main/java/edu/handong/csee/java/lab07/prob3/Attendance.java
+++ src/main/java/edu/handong/csee/java/lab07/prob3/Attendance.java
@@ -10,57 +10,57 @@ import java.util.Random;
  *
  */
 
-public class Attendance {
-	private String name = "";
-	private int year=0;
-	private String IDnumber = "";
-	private int miss =0;
+public class Attendance {	//declare public class named by Attendance
+	private String name = "";	//declare private string  named by name
+	private int year=0;	//declare private int variable named by year and have value zero
+	private String IDnumber = "";	//declare private String  named by IDnumber
+	private int miss =0;	//declare private int variable named by miss and have value zero
 
 
-	public Attendance(){
-		this.name="Null";
-		this.year=0;
-		this.IDnumber="Null";
-		this.miss=0;
+	public Attendance(){	//declare public method named by Attendance
+		this.name="Null";	//by using this comment, named have Null value
+		this.year=0;	//by using this comment, year has zero value 
+		this.IDnumber="Null";	//by using this comment, IDnumber have Null value
+		this.miss=0;	//by using comment, miss has zero value
 	}
 
-	public Attendance(String name, int year, String IDnumber, int miss)
+	public Attendance(String name, int year, String IDnumber, int miss)	//declare public method including String and year member
 	{
-		this.name=name;
-		this.year=year;
-		this.IDnumber=IDnumber;
-		this.miss=miss;
+		this.name=name;	//this.name equal name value
+		this.year=year;	//this.year equal year value
+		this.IDnumber=IDnumber;	//this.IDnumber equal IDnumber value
+		this.miss=miss;	// this.miss equal miss value 
 	}
 
-	public String get_name() {
-		return this.name;
+	public String get_name() {	//declare public string method named by get_name
+		return this.name;	//return this.name
 	}
 
-	public int get_year() {
-		return this.year;
+	public int get_year() {	//declare public integer method named by get_year
+		return this.year;	//return this.year
 	}
 
-	public String get_IDnumber() {
-		return this.IDnumber;
+	public String get_IDnumber() {	//declare public string method named by get_IDnumber
+		return this.IDnumber;	//return this.IDnumber
 	}
 
-	public int miss() {
-		return this.miss;
+	public int get_miss() {	//declare public integer method named by get_miss
+		return this.miss;	//return this.miss
 	}
 
-	public void set_name(String name) {
-		this.name =name;
+	public void set_name(String name) {	//declare public void method named by set_name (including String name)
+		this.name =name;	//this.name equal name
 	}
 
-	public void set_year(int year) {
-		this.year=year;
+	public void set_year(int year) {	//declare public void method named by set_year(including int year)
+		this.year=year;	//this.year equal year
 	}
 
-	public void set_IDnumber( String IDnumber) {
-		this.IDnumber=IDnumber;
+	public void set_IDnumber( String IDnumber) {	//declare public void method named by set_IDnumber(including String IDnumber)
+		this.IDnumber=IDnumber;	//this.IDnumber equal IDnumber
 	}
 
-	public void set_miss(int miss) {
-		this.miss=miss;
+	public void set_miss(int miss) {	//declare public void method named by set_miss(including int miss)
+		this.miss=miss;	//this.miss equal miss
 	}
 }
