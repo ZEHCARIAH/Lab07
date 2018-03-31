5751613e30afe1f173d6ed12c831507b496d7be9
ZEHCARIAH <21700570@handong.edu>
2018-04-01 00:06:13 +0900

    add class and add main method

---
+++ src/main/java/edu/handong/csee/java/lab07/prob2/FriedChickenMenu_Main.java
@@ -0,0 +1,18 @@
+package edu.handong.csee.java.lab07.prob2;
+
+public class FriedChickenMenu_Main {
+
+	public static void main(String[] args) {	
+		FriedChickenMenu menu1=new FriedChickenMenu("Cheese_mustard",16000.00,5);	
+		FriedChickenMenu menu2=new FriedChickenMenu("Honey_musatard",16000.00,5);	
+		FriedChickenMenu menu3=new FriedChickenMenu("Spicey_chicken",16000.00,5);	
+
+		menu1.set_stars(3);	
+		menu2.set_stars(4);	
+		menu3.set_stars(1);	
+		
+		System.out.println(menu1.get_name()+"'s rating is "+menu1.get_stars());	
+		System.out.println(menu2.get_name()+"'s rating is "+menu2.get_stars());	
+		System.out.println(menu3.get_name()+"'s rating is "+menu3.get_stars());	
+	}
+}
