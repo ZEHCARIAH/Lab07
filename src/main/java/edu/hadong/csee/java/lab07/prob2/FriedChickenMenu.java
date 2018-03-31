ecb21d3ee65d31782c489e2b608c43591f2f5a66
ZEHCARIAH <21700570@handong.edu>
2018-03-31 14:31:56 +0900

    fix javadoc and fix some errors

--- src/main/java/edu/hadong/csee/java/lab07/prob2/FriedChickenMenu.java
+++ src/main/java/edu/hadong/csee/java/lab07/prob2/FriedChickenMenu.java
@@ -4,6 +4,7 @@ package edu.hadong.csee.java.lab07.prob2;
  * This class has public string, double, integer and void method.</br>
  * public class declare each member to use it</br>
  * public method declare each memeber's value</br>
+ * another public method include string,double,int members</br>
  * Each public void method call each member</br>
  * @author MG
  *
@@ -21,6 +22,12 @@ public class FriedChickenMenu {	//declare public class FriedChickenMenu
 		this.price=0.0;	//by using this, price's value can has 0.0
 		this.stars=0;	//by using this, stars's value can has 0
 	}
+	public FriedChickenMenu(String name, double price, int stars) {	//declare public method including 
+		this.name=name;		//this.name equal name
+		this.price=price;	//this.price equal price;
+		this.stars=stars;	//this.stars equal stars
+	}
+	
 	public String get_name() 	//declare public String method
 	{
 		return this.name;	//return this.name;
